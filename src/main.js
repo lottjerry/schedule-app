0faf3dfe3f9b85762911f466cc4fba1ec74b3f3f
Jerry Lott <jj@Jerrys-Mac-mini.local>
2023-05-07 17:09:05 -0500

    First commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-createApp(App).use(store).use(router).mount("#app");
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+  iconfont: 'md',
+	components,
+	directives,
+});
+
+createApp(App).use(store).use(router).use(vuetify).mount('#app');
